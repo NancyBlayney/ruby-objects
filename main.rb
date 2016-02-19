4c17765f7b2f2363db243ccf1b224cc61cc48edf
Nancy Blayney <nblayney@g.cofc.edu>
2016-02-18 21:27:52 -0500

    completed homework

--- main.rb
+++ main.rb
@@ -23,6 +23,15 @@ class Calculator
 
 end
 
+calc = Calculator.new(12, 3)
+
+calc.add
+calc.subtract
+calc.multiply
+calc.divide
+
+
+
 
 
 class Elevator
@@ -35,17 +44,43 @@ class Elevator
 	def go_up(a)
 		if a > @floor
 			@floor = a
+		elsif a == @floor
+			puts "You're already on floor #{@floor}, boofus"
+		else
+			puts "That floor is below you. Please you the go_down function."
 		end
 	end
 
 	def go_down(a)
 		if a < @floor
 			@floor = a
+		elsif a == @floor
+			puts "You're already on floor #{@floor}, boofus"
+		else
+			puts "That floor is above you. Please you the go_up function."
 		end
 	end
 
 	def greet
-		return "Welcome to floor number #{@floor}"
+		puts "Welcome to floor number #{@floor}"
+	end
+
+	def play_music
+		puts "Music starts playing. It's a power ballad."
 	end
 
 end
+
+
+elevator = Elevator.new
+
+puts elevator.floor
+elevator.go_up(12)
+elevator.greet
+elevator.go_up(8)
+elevator.go_down(8)
+elevator.greet
+elevator.go_down(8)
+elevator.play_music
+
+
